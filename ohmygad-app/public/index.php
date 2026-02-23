b1ff1f53cae90bafc4d6e83cecbc67d3e815078f
Axo_Kailerr <192589450+sj-abiva@users.noreply.github.com>
2026-02-23 15:57:05 +0800

    html base template
    Started making the base template html

---
+++ ohmygad-app/public/index.php
@@ -0,0 +1,41 @@
+<!-- I was thinking we make this the main template for the different pages? -->
+<!-- Since the diff pages (mostly) contain the same kinds of elements and stuff  -->
+
+<!-- uhh -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>OhMyGAD!</title>
+
+        <!-- maybe multiple css files linked idk -->
+        <link rel="stylesheet" href=" ">
+        <link rel="stylesheet" href=" ">
+
+        
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+        <div class="header" id="home-header">
+
+        </div>
+
+        <div class="container" id="home-container">
+
+            <div class="sidebar" id="home-sidebar">
+                <p>sidebar goes here</p>
+            </div>
+
+            <div class="body" id="home-body">
+                <p>body goes here</p>
+            </div>
+
+        </div>
+
+        <div class="footer" id="home-footer">
+                <p>footer goes here</p>
+        </div>
+    </body>
+</html>
