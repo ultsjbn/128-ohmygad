0e6a476038abc7573a74b0aa44d8a2ad793c1aef
JB <99584741+ultsjbn@users.noreply.github.com>
2026-02-28 19:36:27 +0800

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -12,7 +12,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
           className="bg-transparent"
           left={
             <div className="flex items-center gap-2">
-              <InputText defaultValue="Search" />
+              <InputText placeholder="Search" />
             </div>
           }
           right={
