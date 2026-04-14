7be366d7c57f7cae4275ae7bf938cd8adf6737f8
Duncan <princedunc1214@gmail.com>
2026-04-14 20:44:23 +0800

    forced color for password input to be light
    changed color to white background with black text
    
    password won't auto fill anymore

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -925,10 +925,10 @@ export default function EventsPage() {
                 type="password"
                 value={deletePassword}
                 onChange={(e) => setDeletePassword(e.target.value)}
-                className="w-full px-3 py-2 border border-[rgba(45,42,74,0.2)] rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)]"
+                className="w-full px-3 py-2 border border-[rgba(45,42,74,0.2)] rounded-lg bg-white text-black focus:outline-none focus:ring-2 focus:ring-[var(--pink-light)] disabled:opacity-50"
                 placeholder="Password"
                 disabled={!!deletingId}
-                autoComplete="off"
+                autoComplete="new-password"
               />
             </div>
           </div>
