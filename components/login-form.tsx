7d66d93e824f1fcda490d91fbeff7cd39701a476
JB <99584741+ultsjbn@users.noreply.github.com>
2026-04-23 15:14:33 +0800

    feat: sign up with Google, and Google icon

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -93,7 +93,7 @@ export function LoginForm({
         <div className="input-wrap">
           <label htmlFor="email" className="label">Email</label>
           <div className="input-icon-wrap">
-            <Mail className="input-prefix-icon w-2 h-2" />
+            <Mail className="input-prefix-icon w-4 h-4" />
             <input
               id="email"
               type="email"
@@ -110,7 +110,7 @@ export function LoginForm({
         <div className="input-wrap">
           <label htmlFor="password" className="label">Password</label>
           <div className="input-icon-wrap">
-            <Lock className="input-prefix-icon w-2 h-2" />
+            <Lock className="input-prefix-icon w-4 h-4" />
             <input
               id="password"
               type={showPassword ? "text" : "password"}
@@ -126,7 +126,7 @@ export function LoginForm({
               className="absolute right-3 top-1/2 -translate-y-1/2 text-[var(--gray)] hover:text-[var(--text)] transition-colors"
               tabIndex={-1}
             >
-              {showPassword ? <EyeOff className="w-2 h-2" /> : <Eye className="w-2 h-2" />}
+              {showPassword ? <EyeOff className="w-4 h-4" /> : <Eye className="w-4 h-4" />}
             </button>
           </div>
           <div className="flex justify-end">
@@ -157,9 +157,18 @@ export function LoginForm({
 
       </form>
 
+    {/* Sign in With Google */}
+    {/* https://developers.google.com/identity/branding-guidelines */}
       <form action={signInWithGoogle} className="flex flex-col gap-4 pt-4 items-center">
         <hr className="w-full border-[var(--gray)] m-4" />
         <Button type="submit" variant="ghost" className="w-full">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlnsXlink="http://www.w3.org/1999/xlink" style={{ display: 'block', width: 20, height: 20, flexShrink: 0 }}>
+                  <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                  <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                  <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                  <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                  <path fill="none" d="M0 0h48v48H0z"></path>
+              </svg>
           Sign in with Google
         </Button>
       </form>
